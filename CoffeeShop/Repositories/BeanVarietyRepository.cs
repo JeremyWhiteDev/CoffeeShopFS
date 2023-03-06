6335ff69399608581763721e2b1cd4b42b084224
Jeremy White <jcwhite414@gmail.com>
2023-03-06 17:53:28 -0600

    Add project files.

---
+++ CoffeeShop/Repositories/BeanVarietyRepository.cs
@@ -0,0 +1,41 @@
+﻿using CoffeeShop.Models;
+
+namespace CoffeeShop.Repositories
+{
+    public class BeanVarietyRepository : IBeanVarietyRepository
+    {
+
+        private readonly string _connectionString;
+        public BeanVarietyRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public List<BeanVariety> GetAll()
+        {
+            return _beanVarieties;
+        }
+
+        public BeanVariety Get(int id)
+        {
+            return _beanVarieties.Find(x => x.Id == id);
+        }
+
+        public void Add(BeanVariety variety)
+        {
+            _beanVarieties.Add(variety);
+        }
+
+        public void Update(BeanVariety variety)
+        {
+            var foundBeanVariety = _beanVarieties.Find(b => b.Id == variety.Id);
+
+            foundBeanVariety = variety;
+        }
+
+        public void Delete(int id)
+        {
+            _beanVarieties.Remove(Get(id));
+        }
+    }
+}
---
+++ js-app/main.js
@@ -0,0 +1,52 @@
+﻿const url = "https://localhost:5001/api/beanvariety/";
+
+
+
+const beanListContainer = document.getElementById("beanList")
+
+const beanFormContainer = document.getElementById("beanForm")
+
+const formValues = {
+    beanName: "",
+    beanRegion: "",
+    beanNotes: ""
+}
+
+
+
+
+
+const form = `    
+<form>
+    <fieldset>
+        <label for="beanName">Bean Name</label>
+        <input type="text" placeholder="Enter bean name" name="beanName" onChange=`(e) => updateBeanName(e)` value=${formValues.beanName}/>
+    </fieldset>
+    <fieldset>
+        <label for="beanRegion">Bean Region</label>
+        <input type="text" placeholder="Enter bean region" name="beanRegion" value=${formValues.beanRegion}/>
+    </fieldset>
+    <fieldset>
+        <label for="beanNotes">Bean Notes</label>
+        <textarea type="textarea" placeholder="Enter any notes" name="beanNotes" value=${formValues.beanNotes}></textarea>
+    </fieldset>
+    <button type="submit">Submit Form</button>
+</form>`
+
+
+const button = document.querySelector("#run-button");
+button.addEventListener("click", () => {
+    getAllBeanVarieties()
+        .then(beanVarieties => {
+            console.log(beanVarieties)
+            htmlString = ""
+        beanVarieties.map(variety => htmlString +=  `<h1>${variety.name}</h1>`);
+        beanListContainer.innerHTML = htmlString;
+        })
+});
+
+
+
+function getAllBeanVarieties() {
+    return fetch(url).then(resp => resp.json());
+}
