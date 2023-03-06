5a02ca307b5fb76dc4e0995b8a57e08873435aab
Jeremy White <jcwhite414@gmail.com>
2023-03-06 17:57:57 -0600

    Initial SQL server project import

---
+++ CoffeeShop_DB/dbo/Tables/BeanVariety.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[BeanVariety] (
+    [Id]     INT           IDENTITY (1, 1) NOT NULL,
+    [Name]   VARCHAR (50)  NOT NULL,
+    [Region] VARCHAR (255) NOT NULL,
+    [Notes]  TEXT          NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ CoffeeShop_DB/dbo/Tables/Coffee.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Coffee] (
+    [Id]            INT          IDENTITY (1, 1) NOT NULL,
+    [Title]         VARCHAR (50) NOT NULL,
+    [BeanVarietyId] INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Coffee_BeanVariety] FOREIGN KEY ([BeanVarietyId]) REFERENCES [dbo].[BeanVariety] ([Id])
+);
+
