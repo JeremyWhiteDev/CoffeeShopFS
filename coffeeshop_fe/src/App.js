b332d3970879a2f4f14eb15f9419723b2a65ca23
Jeremy White <jcwhite414@gmail.com>
2023-03-06 20:16:37 -0600

    front end added to project

---
+++ coffeeshop_fe/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import CoffeeList from './components/CoffeeList';
+
+function App() {
+    return (
+        <main className="pt-20 min-h-screen bg-neutral-50 dark:bg-slate-900 pb-16">
+           
+            <h1 className="text-8xl pl-4 dark:text-white max-w-xl md:max-w-screen-xl mb-6">Coffee Shop</h1>
+        <div className="p-4">
+            <CoffeeList />
+            </div>
+        </main>
+  );
+}
+
+export default App;
