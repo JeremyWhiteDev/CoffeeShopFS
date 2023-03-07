32980cc6b5f949c2ba43efa08887fc533e060725
Jeremy White <jcwhite414@gmail.com>
2023-03-06 20:17:09 -0600

    bean repo CRUD accomplished

--- CoffeeShop/Repositories/BeanVarietyRepository.cs
+++ CoffeeShop/Repositories/BeanVarietyRepository.cs
@@ -1,41 +1,154 @@
 ﻿using CoffeeShop.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.VisualBasic;
 
-namespace CoffeeShop.Repositories
+namespace CoffeeShop.Repositories;
+
+public class BeanVarietyRepository : IBeanVarietyRepository
 {
-    public class BeanVarietyRepository : IBeanVarietyRepository
+
+    private readonly string _connectionString;
+    public BeanVarietyRepository(IConfiguration configuration)
     {
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
 
-        private readonly string _connectionString;
-        public BeanVarietyRepository(IConfiguration configuration)
+    private SqlConnection Connection
+    {
+        get { return new SqlConnection(_connectionString); }
+    }
+
+    public List<BeanVariety> GetAll()
+    {
+        using (var conn = Connection)
         {
-            _connectionString = configuration.GetConnectionString("DefaultConnection");
-        }
-
-        public List<BeanVariety> GetAll()
-        {
-            return _beanVarieties;
-        }
-
-        public BeanVariety Get(int id)
-        {
-            return _beanVarieties.Find(x => x.Id == id);
-        }
-
-        public void Add(BeanVariety variety)
-        {
-            _beanVarieties.Add(variety);
-        }
-
-        public void Update(BeanVariety variety)
-        {
-            var foundBeanVariety = _beanVarieties.Find(b => b.Id == variety.Id);
-
-            foundBeanVariety = variety;
-        }
-
-        public void Delete(int id)
-        {
-            _beanVarieties.Remove(Get(id));
+            conn.Open();
+            using (var cmd = conn.CreateCommand()) 
+            {
+                cmd.CommandText = "SELECT Id, Name, Region, Notes FROM BeanVariety;";
+                var reader = cmd.ExecuteReader();
+                var varieties = new List<BeanVariety>();
+                while (reader.Read())
+                {
+                    var variety = new BeanVariety()
+                    {
+                        Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                        Name = reader.GetString(reader.GetOrdinal("Name")),
+                        Region = reader.GetString(reader.GetOrdinal("Region"))
+                    };
+                    if (!reader.IsDBNull(reader.GetOrdinal("Notes")))
+                    {
+                        variety.Notes = reader.GetString(reader.GetOrdinal("Notes"));
+                    }
+                    varieties.Add(variety);
+                }
+                reader.Close();
+                return varieties;
+            }
         }
     }
+
+    public BeanVariety Get(int id)
+    {
+      using (var conn = Connection)
+        {
+            conn.Open();
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"
+                    SELECT Id, [Name], Region, Notes
+                    FROM BeanVariety
+                    WHERE Id = @id;";
+                cmd.Parameters.AddWithValue("@id", id);
+                var reader = cmd.ExecuteReader();
+                BeanVariety variety = null;
+
+                if (reader.Read())
+                {
+                    variety = new BeanVariety()
+                    {
+                        Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                        Name = reader.GetString(reader.GetOrdinal("Name")),
+                        Region = reader.GetString(reader.GetOrdinal("Region")),
+                    };
+                    if (!reader.IsDBNull(reader.GetOrdinal("Notes")))
+                    {
+                        variety.Notes = reader.GetString(reader.GetOrdinal("Notes"));
+                    }
+                }
+                reader.Close() ;
+                return variety;
+            }
+        }
+    }
+
+    public void Add(BeanVariety variety)
+    {
+        using (var conn = Connection)
+        {
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"
+                    INSERT INTO BeanVariety ([Name], Region, Notes)
+                    OUTPUT INSERTED.Id
+                    VALUES (@name, @region, @notes)";
+                cmd.Parameters.AddWithValue("@name", variety.Name);
+                cmd.Parameters.AddWithValue("@region", variety.Region);
+                if (variety.Notes == null)
+                {
+                    cmd.Parameters.AddWithValue("@notes", DBNull.Value);
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("@notes", variety.Notes);
+                }
+                variety.Id = (int)cmd.ExecuteScalar();
+            }
+        }
+    }
+
+    public void Update(BeanVariety variety)
+    {
+        using (var conn = Connection)
+        {
+            conn.Open();
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"
+                        UPDATE BeanVariety 
+                           SET [Name] = @name, 
+                               Region = @region, 
+                               Notes = @notes
+                         WHERE Id = @id";
+                cmd.Parameters.AddWithValue("@id", variety.Id);
+                cmd.Parameters.AddWithValue("@name", variety.Name);
+                cmd.Parameters.AddWithValue("@region", variety.Region);
+                if (variety.Notes == null)
+                {
+                    cmd.Parameters.AddWithValue("@notes", DBNull.Value);
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("@notes", variety.Notes);
+                }
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public void Delete(int id)
+    {
+        using (var conn = Connection)
+        {
+            conn.Open();
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "DELETE FROM BeanVariety WHERE Id = @id";
+                cmd.Parameters.AddWithValue("@id", id);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+}
 }
